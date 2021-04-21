3867c81f628da5142af591b086eba3a148ed4121
David Carmo <davidsc2812@gmail.com>
2021-04-21 19:29:03 -0300

    Adicao do axios no projeto para substituir a utilizacao do fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,36 @@
 //SPA - chamadas a api fazendo uso do useEffect
 //SSR
 //SSG
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  episodes: Array<Episode>; //pode ser usado tbm Episode[]; 
+};
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1> index </h1>
-    <p>{JSON.stringify(props.episodes)}</p>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const {data} = await api.get(`episodes`,{ 
+  params: {
+    _limit: 12,
+    _sort: 'published_at', 
+    _order: 'desc'
+  }});
+  
 
   return {
     props: {
@@ -21,4 +38,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
