3b434e4d88fee7f79f790565ae286a8b5292875d
David Carmo <davidsc2812@gmail.com>
2021-04-21 20:05:33 -0300

    Adicao da funcao para conversao da duracao de number para string e alteracao na forma de retorno dos dados da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,23 @@
 //SSG
 import { GetStaticProps } from "next";
 import { api } from "../services/api";
-
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 type Episode = {
-  id: string;
-  title: string;
-  members: string;
+  id:string ,
+  title:string ,
+  thumbnail:string ,
+  members:string ,
+  publishedAt:string ,  
+  duration: number,
+  durationAsString:string,
+  description:string ,
+  url:string,
 };
 
 type HomeProps = {
-  episodes: Array<Episode>; //pode ser usado tbm Episode[]; 
+  episodes: Array<Episode>; //pode ser usado tbm Episode[];
 };
 
 export default function Home(props: HomeProps) {
@@ -24,17 +32,33 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const {data} = await api.get(`episodes`,{ 
-  params: {
-    _limit: 12,
-    _sort: 'published_at', 
-    _order: 'desc'
-  }});
-  
+  const { data } = await api.get(`episodes`, {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }), // publishedAt come from the api as published_at
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
